36ec1ae56a83bc9f8a527668f22d3fb6457e7433
Adrianjd <adriankk7@gmail.com>
2024-03-09 16:20:19 -0500

    Write out code for getComputerChoice function in javascript.js
    This is the computer randomly returning 'rock', 'paper', or 'scissor'

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch (randomNumber) {
+        case 1:
+            return 'rock';
+            break;
+        case 2:
+            return 'paper';
+            break;
+        case 3:
+            return 'scissors';
+    }
+}
+
