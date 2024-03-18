01dfd4eb223ea5ea5edb1e2c2ee5530916a7bfc0
Adrianjd <adriankk7@gmail.com>
2024-03-18 19:49:56 -0400

    Refactor event listener function

--- javascript.js
+++ javascript.js
@@ -15,44 +15,40 @@ function getComputerChoice() {
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
+    const playerChoice = button.classList.value;
+    
     button.addEventListener("click", () => {
-        const choice = button.class;
-        return choice;
-    });
+        const computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice);
+    })
 });
 
 function playRound(playerSelection, computerSelection) {
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            if (button.class === computerSelection) {
-                return `It's a tie! You and Computer both chose ${button.class}!`
-            }
-        })
-    });
+    
 
     if (playerSelection === computerSelection) {
         return `It's a tie! You and Computer both chose ${playerSelection}!`
-    } else if (text === "rock" && computerSelection === "paper") {
-        return `You Lose! Computer chose ${computerSelection}, and ${computerSelection} beats ${text}!`
-    } else if (text === "rock" && computerSelection === "scissors") {
-        return `You Win! Computer chose ${computerSelection}, and ${text} beats ${computerSelection}!`
-    } else if (text === "paper" && computerSelection === "scissors") {
-        return `You Lose! Computer chose ${computerSelection}, and ${computerSelection} beats ${text}!`
-    } else if (text === "paper" && computerSelection === "rock") {
-        return `You Win! Computer chose ${computerSelection}, and ${text} beats ${computerSelection}!`
-    } else if (text === "scissors" && computerSelection === "rock") {
-        return `You Lose! Computer chose ${computerSelection}, and ${computerSelection} beats ${text}!`
-    } else if (text === "scissors" && computerSelection === "paper") {
-        return `You Win! Computer chose ${computerSelection}, and ${text} beats ${computerSelection}!`
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return `You Lose! Computer chose ${computerSelection}, and ${computerSelection} beats ${playerSelection}!`
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `You Win! Computer chose ${computerSelection}, and ${playerSelection} beats ${computerSelection}!`
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return `You Lose! Computer chose ${computerSelection}, and ${computerSelection} beats ${playerSelection}!`
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return `You Win! Computer chose ${computerSelection}, and ${playerSelection} beats ${computerSelection}!`
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return `You Lose! Computer chose ${computerSelection}, and ${computerSelection} beats ${playerSelection}!`
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `You Win! Computer chose ${computerSelection}, and ${playerSelection} beats ${computerSelection}!`
     }
 }
 
 function playGame() {
-    // let answer = prompt("Let's play! Enter 'rock', 'paper', or 'scissors'!")
+    
     // for (let i = 0; i < 5; i++) {
-    //     console.log(`Round ${i+1}:`, playRound(answer, getComputerChoice()));
+    console.log("Round 1:", playRound(playerChoice(), getComputerChoice()));
     // }
     console.log("Thanks for playing!")
 }
 
-playGame()
+// playGame()
